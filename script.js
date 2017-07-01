ba5f546e98dfc16a9c18e0b86e32cc5d17442e8d
ArtemNovorai <artnovorai@gmail.com>
2017-07-01 23:41:49 +0300

    added new elements of colors table in code

--- script.js
+++ script.js
@@ -5,15 +5,23 @@ document.write("<table border='1px'>");
 for(var i = 1; i < 10; i++) {
   
   if (i == 1){
-    color_tr = "#ba59a0";
+    color_tr = "green";
   } 
   else {
-    color_td = "#e5598c";  
+    color_tr = "yellow";  
   }
   
   document.write("<tr style='height:30px;background-color:" + color_tr + "'>");
   for(var j = 1; j < 10; j++) {
-     document.write("<td style='width:30px'>" + i*j + "</td>")
+    
+    if (j == 1 || i == 1){
+    color_td = "green";
+  } 
+  else {
+    color_td = "yellow";  
+  }
+    
+     document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>")
   }
   document.write("</tr>");
 }
